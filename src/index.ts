f141bb9c24a1e1f63355af0781574e603256a568
Sudo-Self <sudo-self@proton.me>
2025-06-14 19:54:22 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { R2Explorer } from 'r2-explorer';
 
-export default R2Explorer({
-  readonly: false,
-  basicAuth: {
-    username: 'jroper',
-    password: '1235678'
+export default R2Explorer({ readonly: false, basicAuth: {
+    username: 'jessejesse',
+    password: '123567'
   }
 });
-
