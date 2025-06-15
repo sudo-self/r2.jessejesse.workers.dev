81fba700a0c6b190790d667c0ba89530e68215e9
Sudo-Self <sudo-self@proton.me>
2025-06-14 19:51:11 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer';
 
 export default R2Explorer({
-  // Set to false to allow users to upload files
   readonly: false,
-
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
+  basicAuth: {
+    username: 'jroper',
+    password: '1235678'
+  }
 });
+
